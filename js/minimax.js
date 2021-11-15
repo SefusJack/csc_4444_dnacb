c78dea33d8358ad7bfd750b3c8d8bffe8718262d
Jmccain123 <jmccai8@lsu.edu>
2021-11-14 19:21:15 -0600

    test

--- js/minimax.js
+++ js/minimax.js
@@ -3,16 +3,18 @@ class minimax
   static getMove(boardPosition, possibleMoves){
     var game = new Chess(boardPosition)
     var randomIdx = Math.floor(Math.random() * possibleMoves.length)
+    console.log(possibleMoves)
     game.move(possibleMoves[randomIdx])
     return game.fen()
   }
   
-  function getPieceValue(piece){
-    if (pieace.type === null){return 0}
-    else if (piece.type === 'p'){return 1}
-    else if (piece.type === 'n' || piece.type == 'b'){return 3}
-    else if (piece.type === 'r'){return 5}
-    else if (piece.type === 'q'){return 9}
-    else if (piece.type === 'k'){return 100}
-  }
+  // function getPieceValue(piece){
+  //   if (pieace.type === null){return 0}
+  //   else if (piece.type === 'p'){return 1}
+  //   else if (piece.type === 'n' || piece.type == 'b'){return 3}
+  //   else if (piece.type === 'r'){return 5}
+  //   else if (piece.type === 'q'){return 9}
+  //   else if (piece.type === 'k'){return 100}
+  // }
+
 }
