57fd74e199446e41391e54cd076447e6ae9928ab
SefusJack <46831414+SefusJack@users.noreply.github.com>
2021-11-14 20:01:15 -0600

    fix initial minimax problems
    class was being seen as a typescript

--- js/minimax.js
+++ js/minimax.js
@@ -1,10 +1,11 @@
-class minimax
-{
-  static getMove(boardPosition, possibleMoves){
+function getMinimaxMove(boardPosition, possibleMoves){
     var game = new Chess(boardPosition)
     var randomIdx = Math.floor(Math.random() * possibleMoves.length)
     console.log(possibleMoves)
     game.move(possibleMoves[randomIdx])
+    getPieceValue()
     return game.fen()
   }
+
+function getPieceValue(){
 }
--- js/random.js
+++ js/random.js
@@ -1,6 +1,6 @@
 class random
 {
-	static getMove(boardPosition, possibleMoves){
+	static getRandomMove(boardPosition, possibleMoves){
 		var game = new Chess(boardPosition)
 		var randomIdx = Math.floor(Math.random() * possibleMoves.length)
 		game.move(possibleMoves[randomIdx])
